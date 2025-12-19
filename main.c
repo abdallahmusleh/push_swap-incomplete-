524b3fbee3a01c86b3a82e64b8caf28a1a688415
abdallahmusleh <131618632+abdallahmusleh@users.noreply.github.com>
2025-12-19 22:11:28 +0300

    Change list_a type and improve error reporting
    Refactor list_a from s_node to s_list and update error handling.

--- main.c
+++ main.c
@@ -15,22 +15,29 @@
 int	main(int argc, char **argv)
 {
 	int		i;
-	s_node	*list_a;
+	s_list	*list_a;
 
 	i = 0;
 	if (argc <= 1)
 		return (0);
-	while (list_a->next)
+    // if (argc == 2)
+    // {
+    //     while (list_a->next)
+    //     {
+
+    //     }
+    // }
+	while (*list_a->next)
 	{
 		if (not_number(argv) == 1)
-			return (0);
+			return (write(2, "Error\n", 6););
 		list_a->content = ft_atoi(argv[i]);
 		if (invalid_int(list_a->content) == 1)
-			return (0);
+			return (write(2, "Error\n", 6););
 		if (issorted() == 1)
-			return (0);
+			return (write(2, "Error\n", 6););
 		if (repeated_number() == 1)
-			return (0);
+			return (write(2, "Error\n", 6););
 		list_a = list_a->next;
 	}
 }

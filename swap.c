527f943c7851490aac7ecd0bd35cc96ae11a91d1
abdallahmusleh <131618632+abdallahmusleh@users.noreply.github.com>
2025-12-20 22:44:45 +0300

    Refactor swap function and update related functions

--- swap.c
+++ swap.c
@@ -1,30 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: abmusleh <abmusleh@student.42.fr>          +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
+/*   Created: 2025/12/20 17:54:54 by abmusleh          #+#    #+#             */
+/*   Updated: 2025/12/20 17:54:54 by abmusleh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void swap(s_list **stack)
+
+static void	swap(s_list **stack)
 {
-    if(!stack || !*stack || !*stack->next)
-        return(0);
-    s_list *tmp;
-    tmp->content = *stack->content;
-    *stack->content = *stack->next->content;
-    *stack->next->content = tmp->content;
+	if (!stack || !*stack || !(*stack)->next)
+		return ;
+	int tmp;
+    tmp = (*stack) -> content;
+	(*stack)->content = (*stack)->next->content;
+	(*stack)->next->content = tmp;
 }
 
-void sa(s_list **stack_a)
+void	sa(s_list **stack_a)
 {
-    swap(stack_a);
-    write (1, "sa\n", 3);
+	swap(stack_a);
+	write(1, "sa\n", 3);
 }
 
-void sb(s_list **stack_b)
+void	sb(s_list **stack_b)
 {
-    swap(stack_b);
-    write (1, "sb\n", 3);
+	swap(stack_b);
+	write(1, "sb\n", 3);
 }
 
-void ss(s_list **stack_a, s_list **stack_b)
+void	ss(s_list **stack_a, s_list **stack_b)
 {
-    swap(stack_a);
-    swap(stack_b);
-    write (1, "ss\n", 3);
+	swap(stack_a);
+	swap(stack_b);
+	write(1, "ss\n", 3);
 }

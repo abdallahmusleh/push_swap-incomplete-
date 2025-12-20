e9ad93c12c273ab9521a447b70b0d00cd6bad57c
abdallahmusleh <131618632+abdallahmusleh@users.noreply.github.com>
2025-12-20 22:43:36 +0300

    Refactor reverse_rotate functions and add comments

--- reverse_rotate.c
+++ reverse_rotate.c
@@ -1,38 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: abmusleh <abmusleh@student.42.fr>          +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
+/*   Created: 2025/12/20 17:54:31 by abmusleh          #+#    #+#             */
+/*   Updated: 2025/12/20 17:54:31 by abmusleh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void reverse_rotate(s_list **stack)
-{
-    if (!stack || !*stack)//check extras?
-        return(0);
-    s_list *tmp;
-    s_list *tmp2;
 
-    tmp = *stack;
-    tmp2 = *stack;
-    while (tmp->next)
-        tmp = tmp->next;
-    tmp->next = *stack;
-    *stack = tmp;
-    while (tmp2->next != tmp)
-        tmp2 = tmp->next;
-    tmp2->next = NULL;
+static void	reverse_rotate(s_list **stack)
+{
+	if (!stack || !*stack || !(*stack)->next)
+		return ;
+	s_list *tmp;
+	s_list *tmp2;
+
+	tmp = *stack;
+	tmp2 = *stack;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = *stack;
+	*stack = tmp;
+	while (tmp2->next != tmp)
+		tmp2 = tmp2->next;
+	tmp2->next = NULL;
 }
 
-void rra(s_list **stack_a)
+void	rra(s_list **stack_a)
 {
-    reverse_rotate(stack_a);
-    write(1, "rra", 3);
+	reverse_rotate(stack_a);
+	write(1, "rra\n", 4);
 }
 
-void rrb(s_list **stack_b)
+void	rrb(s_list **stack_b)
 {
-    reverse_rotate(stack_b);
-    write(1, "rrb", 3);
+	reverse_rotate(stack_b);
+	write(1, "rrb\n", 4);
 }
 
-void rra(s_list **stack_a, s_list **stack_b)
+void	rrr(s_list **stack_a, s_list **stack_b)
 {
-    reverse_rotate(stack_a);
-    reverse_rotate(stack_b);
-    write(1, "rrr", 3);
+	reverse_rotate(stack_a);
+	reverse_rotate(stack_b);
+	write(1, "rrr\n", 4);
 }

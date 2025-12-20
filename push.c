50dc64ff004a11cdece475891baed8d6945770ca
abdallahmusleh <131618632+abdallahmusleh@users.noreply.github.com>
2025-12-20 22:42:54 +0300

    Refactor pa and pb functions in push.c
    Refactored pa and pb functions for stack manipulation.

--- push.c
+++ push.c
@@ -1,26 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: abmusleh <abmusleh@student.42.fr>          +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
+/*   Created: 2025/12/20 17:54:13 by abmusleh          #+#    #+#             */
+/*   Updated: 2025/12/20 17:54:13 by abmusleh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void pa(s_list **stack_a, s_list **stack_b)
+
+void	pa(s_list **stack_a, s_list **stack_b)
 {
-    if (!*stack_b || !stack_b)
-        return(0);
-    s_list *primary;
-    s_list *tmp;
-    primary = *stack_b;
-    tmp = *stack_a;
-    primary->next = tmp;
-    *stack_a = primary;
-    write (1, "pa", 2);
+	if (!stack_b || !*stack_b)
+		return ;
+	s_list *primary;
+	s_list *tmp;
+	primary = *stack_b;
+	*stack_b = (*stack_b)->next;
+	tmp = *stack_a;
+	primary->next = tmp;
+	*stack_a = primary;
+	write(1, "pa\n", 3);
 }
-void pb(s_list **stack_a, s_list **stack_b)
+void	pb(s_list **stack_a, s_list **stack_b)
 {
-    if (!*stack_a || !stack_a)
-        return(0);
-    s_list *primary;
-    s_list *tmp;
-    primary = *stack_a;
-    tmp = *stack_b;
-    primary->next = tmp;
-    *stack_b = primary;
-    write (1, "pb", 2);
+	if (!stack_a || !*stack_a)
+		return ;
+	s_list *primary;
+	s_list *tmp;
+	primary = *stack_a;
+	*stack_a = (*stack_a)->next;
+	tmp = *stack_b;
+	primary->next = tmp;
+	*stack_b = primary;
+	write(1, "pb\n", 3);
 }

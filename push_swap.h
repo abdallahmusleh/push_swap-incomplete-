832a0f3dc7762a6678406909129fb58a2ec66e0e
abdallahmusleh <131618632+abdallahmusleh@users.noreply.github.com>
2025-12-20 22:42:28 +0300

    Update push_swap.h with new function prototypes
    Added function prototypes for ft_atolong, ft_lstnew, and ft_lstadd_back.

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: abmusleh <abmusleh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 20:03:39 by abmusleh          #+#    #+#             */
-/*   Updated: 2025/12/17 23:02:07 by abmusleh         ###   ########.fr       */
+/*   Updated: 2025/12/20 22:35:49 by abmusleh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <limits.h>
 # include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
 
 typedef struct t_list
@@ -22,4 +23,7 @@ typedef struct t_list
 	long	content;
 	s_list	*next;
 }			s_list;
+long		ft_atolong(const char *nptr);
+s_list		*ft_lstnew(int *content);
+void		*ft_lstadd_back(s_list **list, s_list *new);
 #endif

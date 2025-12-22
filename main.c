f03de4877ede5f5695e27be231ffa3af731b787c
abdallahmusleh <131618632+abdallahmusleh@users.noreply.github.com>
2025-12-22 20:24:16 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abmusleh <abmusleh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 20:06:06 by abmusleh          #+#    #+#             */
-/*   Updated: 2025/12/17 23:10:48 by abmusleh         ###   ########.fr       */
+/*   Updated: 2025/12/21 19:00:14 by abmusleh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	main(int argc, char **argv)
 {
 	int		i;
+	long	value;
 	s_list	*list_a;
 	s_list	*node;
 
@@ -25,14 +26,18 @@ int	main(int argc, char **argv)
 		return (write(2, "Error\n", 6));
 	while (i < argc)
 	{
-		node = ft_lstnew(ft_atolong(argv[i]));
-		ft_lstadd_back(list_a, node);
+		value = ft_atolong(argv[i]);
+		if (invalid_int(value) == 1)
+			return (write(2, "Error\n", 6));
+		node = ft_lstnew(value);//ft_atolong(argv[i])
+		if (!node)
+			return (write(2, "Error\n", 6)); //is this protection necessary?
+		ft_lstadd_back(&list_a, node);
 		i++;
 	}
-	if (invalid_int(list_a->content) == 1)
+	if (repeated_number(list_a) == 1)
 		return (write(2, "Error\n", 6));
-	if (issorted() == 1)
-		return (write(2, "Error\n", 6));
-	if (repeated_number() == 1)
-		return (write(2, "Error\n", 6));
+	if (issorted(list_a) == 1)
+		return (write(2, "Error\n", 6));//or 0;
+	return(0);
 }

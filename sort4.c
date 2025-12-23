4034a3bcfb0be830fb2171af90316f9982f4a64c
abdallahmusleh <131618632+abdallahmusleh@users.noreply.github.com>
2025-12-23 23:04:05 +0300

    Refactor sort4 function to use long types

--- sort4.c
+++ sort4.c
@@ -1,40 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort4.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abmusleh <abmusleh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/23 16:19:30 by abmusleh          #+#    #+#             */
+/*   Updated: 2025/12/23 22:51:46 by abmusleh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void smallest_in_second(s_list **stack_a, s_list **stack_b)
+void	smallest_in_second(s_list **stack_a, s_list **stack_b)
 {
-    sa(stack_a);
-    pb(stack_a, stack_b);
+	sa(stack_a);
+	pb(stack_a, stack_b);
 }
-void smallest_in_third(s_list **stack_a, s_list **stack_b)
+void	smallest_in_third(s_list **stack_a, s_list **stack_b)
 {
-    ra(stack_a);
-    ra(stack_a);
-    pb(stack_a, stack_b);
+	ra(stack_a);
+	ra(stack_a);
+	pb(stack_a, stack_b);
 }
-void smallest_in_last(s_list **stack_a, s_list **stack_b)
+void	smallest_in_last(s_list **stack_a, s_list **stack_b)
 {
-    rra(stack_a);
-    pb(stack_a, stack_b);
+	rra(stack_a);
+	pb(stack_a, stack_b);
 }
 void	sort4(s_list **stack_a, s_list **stack_b)
 {
-	int a;
-    int b;
-    int c;
-    int d;
+	long first;
+	long second;
+	long third;
+	long fourth;
 
-	a = (*stack_a)->content;
-    b = (*stack_a)->next->content;
-	c = (*stack_a)->next->next->content;
-	d = (*stack_a)->next->next->next->content;
-    if (a < b && a < c && a < d)
-        pb(stack_a, stack_b);
-    if (a > b && b < c && b < d)
-        smallest_in_second(stack_a, stack_b);
-    if (a > c && b > c && c < d)
-        smallest_in_third(stack_a, stack_b);
-    if (d < b && d < c && a > d)
-        smallest_in_last(stack_a, stack_b);
-    sort3(stack_a);
-    pa(stack_a, stack_b);
+	first = (*stack_a)->content;
+	second = (*stack_a)->next->content;
+	third = (*stack_a)->next->next->content;
+	fourth = (*stack_a)->next->next->next->content;
+	if (first < second && first < third && first < fourth)
+		pb(stack_a, stack_b);
+	else if (first > second && second < third && second < fourth)
+		smallest_in_second(stack_a, stack_b);
+	else if (first > third && second > third && third < fourth)
+		smallest_in_third(stack_a, stack_b);
+	else if (fourth < second && fourth < third && first > fourth)
+		smallest_in_last(stack_a, stack_b);
+	sort3(stack_a);
+	pa(stack_a, stack_b);
 }

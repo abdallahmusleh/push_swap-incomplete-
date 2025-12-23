99d61b4f3dfae5f0df94a38c13d678608d77e1fe
abdallahmusleh <131618632+abdallahmusleh@users.noreply.github.com>
2025-12-23 23:02:34 +0300

    Refactor sort3 function for improved logic

--- sort2and3.c
+++ sort2and3.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort2to5.c                                         :+:      :+:    :+:   */
+/*   sort2and3.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abmusleh <abmusleh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 19:35:06 by abmusleh          #+#    #+#             */
-/*   Updated: 2025/12/21 19:58:20 by abmusleh         ###   ########.fr       */
+/*   Updated: 2025/12/23 22:51:44 by abmusleh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,29 @@ void	sort2(s_list **list)
 {
 	sa(list);
 }
-void	sort3(s_list **list, s_list **list2)
+void	sort3(s_list **list)
 {
 	if ((*list)->content < (*list)->next->content
 		&& (*list)->content < (*list)->next->next->content)
 	{
-		pb(list, list2);
-		sa(list);
-		pa(list, list2);
-	}
-	if ((*list)->content > (*list)->next->content
-		&& (*list)->content < (*list)->next->next->content)
-		sa(list);
-	if ((*list)->content < (*list)->next->content
-		&& (*list)->content > (*list)->next->next->content)
 		rra(list);
-	if ((*list)->content > (*list)->next->content
-		&& (*list)->content > (*list)->next->next->content
-		&& (*list)->next->content > (*list)->next->next->content)
+		sa(list);
+	}
+	else if ((*list)->content > (*list)->next->content
+			&& (*list)->content < (*list)->next->next->content)
+		sa(list);
+	else if ((*list)->content < (*list)->next->content
+			&& (*list)->content > (*list)->next->next->content)
+		rra(list);
+	else if ((*list)->content > (*list)->next->content
+			&& (*list)->content > (*list)->next->next->content
+			&& (*list)->next->content > (*list)->next->next->content)
 	{
 		ra(list);
 		sa(list);
 	}
-	if ((*list)->content > (*list)->next->content
-		&& (*list)->content > (*list)->next->next->content
-		&& (*list)->next->content < (*list)->next->next->content)
+	else if ((*list)->content > (*list)->next->content
+			&& (*list)->content > (*list)->next->next->content
+			&& (*list)->next->content < (*list)->next->next->content)
 		ra(list);
 }
